8fbcecd685c0505ca487d15cd4524b6b02e34f9f
josedomingos919 <josedomingos919@gmail.com>
2025-12-30 11:10:35 +0100

    feat: init todo list with angular 20

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('todo_list_angular_spring');
+
+  todoList: TodoItem[] = [];
+  newTask: string = '';
 }
